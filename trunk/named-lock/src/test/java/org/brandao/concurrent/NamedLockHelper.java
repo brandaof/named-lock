3065c92f839627a0a22e0e7c083783cd6b17f512
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-17 17:04:09 +0000

    #

---
+++ trunk/named-lock/src/test/java/org/brandao/concurrent/NamedLockHelper.java
@@ -0,0 +1,145 @@
+package org.brandao.concurrent;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class NamedLockHelper {
+
+	public static class AsyncLock extends Thread{
+		
+		private NamedLock namedLock;
+		
+		private String lockName;
+		
+		private boolean error;
+		
+		private List<Integer> queue;
+		
+		public AsyncLock(NamedLock namedLock, 
+				String lockName, List<Integer> queue){
+			this.namedLock = namedLock;
+			this.lockName  = lockName;
+			this.queue     = queue;
+		}
+		
+		public void run(){
+			try{
+				Serializable ref = this.namedLock.lock(this.lockName);
+				try{
+					this.queue.add(2);
+				}
+				finally{
+					this.namedLock.unlock(ref, this.lockName);
+				}
+				this.error = false;
+			}
+			catch(Throwable e){
+				error = true;
+				e.printStackTrace();
+			}
+		}
+
+		public boolean isError() {
+			return error;
+		}
+		
+	}
+	
+	public static class AsyncTryLock extends Thread{
+		
+		private NamedLock namedLock;
+		
+		private String lockName;
+		
+		private boolean error;
+		
+		private List<Integer> queue;
+		
+		public AsyncTryLock(NamedLock namedLock, 
+				String lockName, List<Integer> queue){
+			this.namedLock = namedLock;
+			this.lockName  = lockName;
+			this.queue     = queue;
+		}
+		
+		public void run(){
+			try{
+				Serializable ref = namedLock.tryLock(this.lockName);
+				if(ref == null)
+					this.queue.add(3);
+				else{
+					try{
+						this.queue.add(2);
+					}
+					finally{
+						namedLock.unlock(ref, this.lockName);
+					}
+				}
+				
+				this.error = false;
+			}
+			catch(Throwable e){
+				this.error = true;
+				e.printStackTrace();
+			}
+		}
+
+		public boolean isError() {
+			return error;
+		}
+		
+	}
+
+	public static class AsyncTryLockTime extends Thread{
+		
+		private NamedLock namedLock;
+		
+		private String lockName;
+		
+		private boolean error;
+		
+		private List<Integer> queue;
+		
+		private long time;
+		
+		private TimeUnit unit;
+		
+		public AsyncTryLockTime(NamedLock namedLock, 
+				String lockName, List<Integer> queue, long time, TimeUnit unit){
+			this.namedLock = namedLock;
+			this.lockName  = lockName;
+			this.queue     = queue;
+			this.time      = time;
+			this.unit      = unit;
+		}
+		
+		public void run(){
+			try{
+				Serializable ref = namedLock.tryLock(this.lockName, this.time, this.unit);
+				if(ref == null)
+					this.queue.add(3);
+				else{
+					try{
+						this.queue.add(2);
+					}
+					finally{
+						namedLock.unlock(ref, this.lockName);
+					}
+				}
+				
+				this.error = false;
+			}
+			catch(Throwable e){
+				this.error = true;
+				e.printStackTrace();
+			}
+		}
+
+		public boolean isError() {
+			return error;
+		}
+		
+	}
+	
+}
