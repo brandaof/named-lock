148aa255c277db03f2bbcd23fe174f0c0c2d4c82
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-17 14:59:44 +0000

    #

--- trunk/named-lock/src/test/java/org/brandao/concurrent/NamedLockTest.java
+++ trunk/named-lock/src/test/java/org/brandao/concurrent/NamedLockTest.java
@@ -95,5 +95,53 @@ public class NamedLockTest extends TestCase{
 		TestCase.assertEquals(2, result.size());
 		TestCase.assertEquals(new Integer(2), result.get(0));
 		TestCase.assertEquals(new Integer(1), result.get(0));
-	}	
+	}
+	
+	public void testSimpleTryLock() throws InterruptedException{
+		
+		final List<Integer> result = new ArrayList<Integer>();
+		
+		final NamedLock namedLock = new NamedLock();
+		
+		Thread th = new Thread(){
+			
+			public void run(){
+				try{
+					Thread.sleep(2000);
+					Serializable ref = namedLock.tryLock("teste");
+					try{
+						if(ref == null)
+							result.add(3);
+						else
+							result.add(2);
+					}
+					finally{
+						namedLock.unlock(ref, "teste");
+					}
+					
+				}
+				catch(Throwable e){
+					e.printStackTrace();
+				}
+			}
+			
+		};
+		
+		th.start();
+		Serializable ref = namedLock.tryLock("teste");
+		try{
+			Thread.sleep(5000);
+			result.add(1);
+		}
+		finally{
+			namedLock.unlock(ref, "teste");
+		}
+		
+		Thread.sleep(2000);
+		
+		TestCase.assertEquals(2, result.size());
+		TestCase.assertEquals(new Integer(3), result.get(0));
+		TestCase.assertEquals(new Integer(1), result.get(0));
+	}
+	
 }
