060066a4d54c3379dcb86635faf90218f21e7f63
afonso <afonso@b5236371-841b-6747-ae89-812dd2fe351b>
2016-08-21 22:11:54 +0000

    #

--- trunk/named-lock/src/test/java/org/brandao/concurrent/DisposableSingletonContextTest.java
+++ trunk/named-lock/src/test/java/org/brandao/concurrent/DisposableSingletonContextTest.java
@@ -1,6 +1,8 @@
 package org.brandao.concurrent;
 
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.TestCase;
 import org.brandao.concurrent.DisposableSingletonContext.ObjectHandler;
@@ -46,6 +48,66 @@ public class DisposableSingletonContextTest extends TestCase{
 		Object newAHandler = ((ObjectHandler) Proxy.getInvocationHandler(a)).object;
 
 		TestCase.assertTrue(aHandler != newAHandler);
+		
+	}
+
+	public void testCreateConcurrent() throws InterruptedException{
+		
+		final List<Object> list = new ArrayList<Object>();
+		
+		final DisposableSingletonContext<String> context = 
+				new DisposableSingletonContext<String>();
+		context.registryBeanDefinition("teste", new ObjectFactory() {
+			
+			public void destroy(Object instance) {
+			}
+			
+			public Object createInstance() {
+				return new Object();
+			}
+			
+		});
+		
+		list.add(context.getBean("A", "teste"));
+		list.add(context.getBean("B", "teste"));
+		
+		Thread th = new Thread(){
+			
+			public void run(){
+				list.add(context.getBean("A", "teste"));
+				list.add(context.getBean("B", "teste"));
+			}
+			
+		};
+		
+		th.start();
+		
+		Thread.sleep(1000);
+		
+		Object reference1 = ((ObjectHandler) Proxy.getInvocationHandler(list.get(0))).object;
+		Object reference2 = ((ObjectHandler) Proxy.getInvocationHandler(list.get(1))).object;
+		Object reference3 = ((ObjectHandler) Proxy.getInvocationHandler(list.get(2))).object;
+		Object reference4 = ((ObjectHandler) Proxy.getInvocationHandler(list.get(3))).object;
+		
+		TestCase.assertEquals(
+				reference1,
+				reference3
+		);
+
+		TestCase.assertEquals(
+				reference2,
+				reference4
+		);
+		
+		list.clear();
+		
+		Runtime.getRuntime().gc();
+
+		Object a = context.getBean("A", "teste");
+		
+		Object reference = ((ObjectHandler) Proxy.getInvocationHandler(a)).object;
+
+		TestCase.assertTrue(reference1 != reference);
 		
 	}
 	
